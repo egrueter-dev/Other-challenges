63a57bd3e06698b6520e5897628c4d91cd897295
Erik Grueter <erikgrueter@gmail.com>
2015-01-21 11:03:33 +0100

    additional coderbyte challenges

---
+++ alphabet_soup.rb
@@ -0,0 +1,12 @@
+# string parameter being passed and return the string with the letters
+# in alphabetical order (ie. hello becomes ehllo)
+
+
+
+def alphabet(str)
+  print str.chars.sort
+
+end
+
+
+puts alphabet('hello')
--- time_convert.rb
+++ time_convert.rb
@@ -1,20 +1,11 @@
 # return the number of hours and minutes the parameter
 # converts to (ie. if num = 63 then the output should be 1:3).
 #
-# def TimeConvert(num)
-#     hour = num/60
-#     minute = num % 60
-#     puts ("#{hour}:#{minute}")
-#
-# end
-
-# TimeConvert(45)
-
-
 def TimeConvert(num)
-  hour = num / 60
-  minute = num % 60
-  puts "#{hour}:#{minute}"
+    hour = num/60
+    minute = num % 60
+    puts ("#{hour}:#{minute}")
+
 end
 
 TimeConvert(45)
